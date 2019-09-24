cb262c7c37294b568d09eb248310d328cda16911
hongmingzhang <hzhangal0330@gmail.com>
2019-09-24 16:54:04 +0800

    test

--- conceptnet/generate_conceptnet_vocab.py
+++ conceptnet/generate_conceptnet_vocab.py
@@ -1,10 +1,23 @@
+from tqdm import tqdm
+
+
+all_words = list()
 with open('conceptnet-assertions-5.6.0.csv.en', 'r') as f:
     for line in f:
         words = line.split('\t')
         tmp_r = words[0]
         tmp_head = words[1]
         tmp_tail = words[2]
-        print(tmp_r, tmp_head, tmp_tail)
-        print(line)
-        break
+        for w in tmp_head.split(' '):
+            all_words.append(w)
+        for w in tmp_tail.split(' '):
+            all_words.append(w)
+
+all_words = list(set(all_words))
+
+with open('concept.txt', 'w') as f:
+    for w in tqdm(all_words):
+        f.write(w)
+        f.write('\n')
+
 print('end')
