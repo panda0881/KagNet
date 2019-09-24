e2313e4e308e5f87676bc8799379e02dd94d5ab1
hongmingzhang <hzhangal0330@gmail.com>
2019-09-24 19:03:02 +0800

    test

--- conceptnet/generate_conceptnet_vocab.py
+++ conceptnet/generate_conceptnet_vocab.py
@@ -1,23 +1,30 @@
 from tqdm import tqdm
 
 
-all_words = list()
+all_concepts = list()
+all_relations = list()
 with open('conceptnet-assertions-5.6.0.csv.en', 'r', encoding='utf-8') as f:
     for line in f:
         words = line.split('\t')
         tmp_r = words[0]
         tmp_head = words[1]
         tmp_tail = words[2]
-        for w in tmp_head.split(' '):
-            all_words.append(w)
-        for w in tmp_tail.split(' '):
-            all_words.append(w)
+        all_concepts.append(tmp_head)
+        all_concepts.append(tmp_tail)
+        all_relations.append(tmp_r)
 
-all_words = list(set(all_words))
+all_concepts = list(set(all_concepts))
+all_relations = list(set(all_relations))
 
 with open('concept.txt', 'w', encoding='utf-8') as f:
-    for w in tqdm(all_words):
+    for w in tqdm(all_concepts):
         f.write(w)
         f.write('\n')
+
+with open('relation.txt', 'w', encoding='utf-8') as f:
+    for w in tqdm(all_relations):
+        f.write(w)
+        f.write('\n')
+
 
 print('end')
