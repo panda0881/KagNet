998ef00e8cef840024e6a5a83fa7854f4333b224
hongmingzhang <hzhangal0330@gmail.com>
2019-09-24 16:40:16 +0800

    test

--- conceptnet/generate_conceptnet_vocab.py
+++ conceptnet/generate_conceptnet_vocab.py
@@ -1,5 +1,9 @@
 with open('conceptnet-assertions-5.6.0.csv.en', 'r') as f:
     for line in f:
+        words = line.split('\t')
+        tmp_r = words[0]
+        tmp_head = words[1]
+        tmp_tail = words[2]
         print(line)
         break
 print('end')
