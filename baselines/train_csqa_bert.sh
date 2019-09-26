0f2a05e1579deebb6a66d4e7c788ea1911408851
hongmingzhang <hzhangal0330@gmail.com>
2019-09-26 23:12:01 +0800

    test

--- baselines/train_csqa_bert.sh
+++ baselines/train_csqa_bert.sh
@@ -8,8 +8,8 @@ python run_csqa_bert.py \
      --do_lower_case \
      --do_eval \
      --data_dir $CSQA_DIR \
-     --train_batch_size 20 \
-     --eval_batch_size 20 \
+     --train_batch_size 15 \
+     --eval_batch_size 15 \
      --learning_rate 1e-4 \
      --num_train_epochs 100.0 \
      --max_seq_length 100 \
